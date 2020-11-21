2f588ba0e79fceb2bbc350b0e877b28f3e87fedf
Fuhito Suguri <bitwalk123@users.noreply.github.com>
2020-11-21 22:51:08 +0900

    Create hello.py
    initial

---
+++ hello.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys
+from PySide2 import QtCore
+from PySide2.QtGui import QFont, QIcon
+from PySide2.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+
+
+class HelloWorld(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.setWindowIcon(QIcon('qt.png'))
+        self.setWindowTitle('Hello, World!')
+        self.initUI()
+        self.show()
+
+    def initUI(self):
+        label = QLabel('こんにちは、世界！')
+        font = QFont()
+        font.setPointSize(24)
+        label.setFont(font)
+        label.setAlignment(QtCore.Qt.AlignCenter)
+
+        layout = QVBoxLayout()
+        layout.addWidget(label)
+        self.setLayout(layout)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    hello = HelloWorld()
+    sys.exit(app.exec_())
