d10639b9715b944ee50a770673044870a8424334
Fuhito Suguri <bitwalk@gmail.com>
2020-11-22 12:41:13 +0900

    decolate label string

--- qt_label.py
+++ qt_label.py
@@ -7,7 +7,7 @@ from PySide2.QtWidgets import QApplication, QWidget, QLabel
 class Example(QWidget):
 
     def initUI(self):
-        lb = QLabel("ラベル", self)
+        lb = QLabel('<font color=#888 size=40>ラベル</font>', self)
         lb.resize(lb.sizeHint())
         lb.move(50, 20)
 
