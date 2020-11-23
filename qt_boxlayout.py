287b6323f8f9ee2c84db6d2e3b3ef8ef5e8c1451
Fuhito Suguri <bitwalk@gmail.com>
2020-11-23 09:22:08 +0900

    add qt_boxlayout.py

---
+++ qt_boxlayout.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys
+from PySide2.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QVBoxLayout
+
+
+class Example(QWidget):
+    def initUI(self):
+        btn_ok = QPushButton('了解')
+        btn_cancel = QPushButton('キャンセル')
+
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(btn_ok)
+        hbox.addWidget(btn_cancel)
+
+        vbox = QVBoxLayout()
+        vbox.addStretch(1)
+        vbox.addLayout(hbox)
+
+        self.setLayout(vbox)
+        self.setGeometry(300, 300, 400, 100)
+        self.setWindowTitle('BoxLayout')
+        self.show()
+
+    def __init__(self):
+        super(Example, self).__init__()
+        self.initUI()
+
+
+def main():
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
