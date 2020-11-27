ae21748ebd071f6586c461a8c3712df3dc771797
Fuhito Suguri <bitwalk@gmail.com>
2020-11-27 14:14:59 +0900

    update

--- qt_togglebutton.py
+++ qt_togglebutton.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
+# reference : https://github.com/andriyantohalim/PySide2_Tutorial
 
 import sys
 from PySide2.QtWidgets import QApplication, QWidget, QPushButton, QFrame
