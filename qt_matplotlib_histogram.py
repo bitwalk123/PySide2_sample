e904f487e2270cacf8d45343cdc35039bb04e92b
Fuhito Suguri <bitwalk@gmail.com>
2020-12-05 09:37:07 +0900

    update

--- qt_matplotlib_histogram.py
+++ qt_matplotlib_histogram.py
@@ -6,14 +6,10 @@ from PySide2 import QtCore, QtGui
 from PySide2.QtWidgets import (
     QApplication,
     QMainWindow,
-    QMdiArea,
-    QMdiSubWindow,
-    QVBoxLayout,
-    QWidget,
 )
 
 from matplotlib.backends.backend_qt5agg import (
-    FigureCanvas,
+    FigureCanvasQTAgg as FigureCanvas,
     NavigationToolbar2QT as NavigationToolbar,
 )
 from matplotlib.figure import Figure
@@ -22,32 +18,32 @@ from matplotlib.figure import Figure
 class Example(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.mdiArea = QMdiArea()
-        self.setCentralWidget(self.mdiArea)
+        self.initUI()
+        self.resize(800, 400)
+        self.show()
 
+    def initUI(self):
         mu, sigma = 100, 15
         x = mu + sigma * np.random.randn(10000)
 
-        self.canvas = FigureCanvas(Figure(figsize=(5, 3)))
-        self.ax = self.canvas.figure.subplots()
-        n, bins, patches = self.ax.hist(
+        canvas = FigureCanvas(Figure(figsize=(5, 3)))
+        ax = canvas.figure.subplots()
+        n, bins, patches = ax.hist(
             x, 50, density=1, facecolor="green", alpha=0.75
         )
-        self.ax.set_xlabel("Smarts")
-        self.ax.set_ylabel("Probability")
-        self.ax.set_title(r"$\mathrm{Histogram\ of\ IQ:}\ \mu=100,\ \sigma=15$")
-        self.ax.axis([40, 160, 0, 0.03])
-        self.ax.grid(True)
 
-        widget = QMainWindow()
-        widget.setCentralWidget(self.canvas)
-        widget.addToolBar(
-            QtCore.Qt.BottomToolBarArea, NavigationToolbar(self.canvas, self)
+        ax.set_xlabel("Smarts")
+        ax.set_ylabel("Probability")
+        ax.set_title(r"$\mathrm{Histogram\ of\ IQ:}\ \mu=100,\ \sigma=15$")
+        ax.axis([40, 160, 0, 0.03])
+        ax.grid(True)
+
+        self.setCentralWidget(canvas)
+        self.addToolBar(
+            QtCore.Qt.BottomToolBarArea,
+            NavigationToolbar(canvas, self)
         )
-        sub_window = QMdiSubWindow()
-        sub_window.setWidget(widget)
-        self.mdiArea.addSubWindow(sub_window)
-        self.show()
+
 
 def main():
     app = QApplication(sys.argv)
