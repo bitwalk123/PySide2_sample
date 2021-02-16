6568b22e5d6419ae9a8cc5cd6452bab963274c9a
Fuhito Suguri <bitwalk@gmail.com>
2021-02-16 16:19:23 +0900

    add qt_console

---
+++ qt_console.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+from PySide2.QtWidgets import QApplication
+from pyqtconsole.console import PythonConsole
+
+
+def greet():
+    print("hello world")
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+
+    console = PythonConsole()
+    console.push_local_ns('greet', greet)
+    console.show()
+    console.eval_in_thread()
+    sys.exit(app.exec_())
