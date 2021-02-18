cc44e8bc2ea2c02eafdeb021dfc259f660fb2ab8
Fuhito Suguri <bitwalk@gmail.com>
2021-02-18 09:40:59 +0900

    update

--- sqlite_test.py
+++ sqlite_test.py
@@ -24,16 +24,15 @@ class Example(QMainWindow):
             self.initDB()
 
         self.initUI()
-        self.setGeometry(100, 100, 600, 1)
         self.setWindowTitle('SQLite Test')
         self.show()
+        self.setGeometry(100, 100, 300, 0)
 
     def initUI(self):
         # menu in
         openFile = QAction('Open', self)
         openFile.setShortcut('Ctrl+O')
         openFile.setStatusTip('Open New File')
-        openFile.triggered.connect(self.showDialog)
 
         menubar = self.menuBar()
         fileMenu = menubar.addMenu('File(&F)')
@@ -50,62 +49,77 @@ class Example(QMainWindow):
         self.get_filelist(combo_file)
         but_file = QPushButton('Open')
         but_file.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
-        but_file.clicked.connect(lambda: self.on_click_open(combo_file))
         grid.addWidget(combo_file, 0, 0)
         grid.addWidget(but_file, 0, 1)
 
-    def showDialog(self):
+        openFile.triggered.connect(lambda: self.showDialog(combo_file))
+        but_file.clicked.connect(lambda: self.on_click_open(combo_file))
+
+    def showDialog(self, combo: QComboBox):
         dialog = QFileDialog()
+        dialog.setNameFilters(['PDF files (*.pdf)'])
         if dialog.exec_():
             fname = dialog.selectedFiles()[0]
             name_file = os.path.basename(fname)
             f = open(fname, 'rb')
             with f:
                 content = f.read()
+
+                # SQLite
                 con = sqlite3.connect(self.dbname)
                 cur = con.cursor()
                 cur.execute("INSERT INTO file VALUES(?, ?);", [name_file, content])
                 con.commit()
                 con.close()
 
+                # update list of QComboBox
+                self.get_filelist(combo)
+
     def get_filelist(self, combo: QComboBox):
         combo.clear()
         combo.clearEditText()
 
+        # SQLite
         con = sqlite3.connect(self.dbname)
         cur = con.cursor()
         cur.execute("SELECT name_file FROM file;")
         out = cur.fetchall()
         con.close()
 
+        # add list of file to QComboBox
         for name_file in out:
             combo.addItem(name_file[0])
 
     def on_click_open(self, combo: QComboBox):
         name_file = combo.currentText()
-        out_file = os.path.join(tempfile.gettempdir(), name_file)
-        print(out_file)
 
+        # set temporary location where to save name_file
+        out_file = os.path.join(tempfile.gettempdir(), name_file)
+
+        # SQLite
         con = sqlite3.connect(self.dbname)
         cur = con.cursor()
         cur.execute("SELECT content FROM file WHERE name_file = ?;", [name_file])
         out = cur.fetchall()
         con.close()
 
+        # save out_file
         with open(out_file, 'wb') as f:
             f.write(out[0][0])
 
+        # open out_file with application
         os.startfile(out_file)
 
     def initDB(self):
-        init_sql = [
+        init_query = [
             'CREATE TABLE file (name_file TEXT UNIQUE, content BLOB)',
         ]
+
         con = sqlite3.connect(self.dbname)
         cur = con.cursor()
 
-        for sql in init_sql:
-            cur.execute(sql)
+        for query in init_query:
+            cur.execute(query)
 
         con.commit()
         con.close()
