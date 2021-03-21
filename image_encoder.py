dc4d4b3d6043499f566d94c6e87c7d67e2daa6b2
Fuhito Suguri <bitwalk@gmail.com>
2021-03-21 21:14:54 +0900

    update

--- image_encoder.py
+++ image_encoder.py
@@ -1,7 +1,8 @@
+from bz2 import compress
 from base64 import b64encode
 
 filename = 'qt.png'
 with open(filename, 'rb') as fileobj:
-    data = b64encode(fileobj.read())
+    data = b64encode(compress(fileobj.read()))
 
 print(data)
