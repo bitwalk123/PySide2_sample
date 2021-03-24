1513bf34b8501d42f9bd3ea6ec825ff72e372991
Fuhito Suguri <bitwalk@gmail.com>
2021-03-24 11:54:46 +0900

    update

--- qt_progressbar.py
+++ qt_progressbar.py
@@ -1,40 +1,67 @@
 #!/usr/bin/env python
 # coding: utf-8
 # Reference:
-# https://codeloop.org/how-to-create-progressbar-in-pyside2/
+# https://stackoverflow.com/questions/59082375/implement-qthread-with-qprogressbar-in-pyside-or-pyqt-during-calculation
 
 import sys
+import time
 from PySide2.QtWidgets import (
     QApplication,
     QLabel,
     QMainWindow,
     QProgressBar,
+    QPushButton,
+    QSizePolicy,
     QStatusBar,
+)
+from PySide2.QtCore import (
+    QThread,
+    Signal,
 )
 
 
 class Example(QMainWindow):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, parent=None):
+        super().__init__(parent)
 
         self.initUI()
-        self.setWindowTitle("ProgressBar")
-        self.setGeometry(100, 100, 500, 400)
+        self.task_def()
+
+        self.setWindowTitle('ProgressBar & Thread')
         self.show()
 
     def initUI(self):
-        statusLabel = QLabel("Showing Progress")
+        self.but = QPushButton("Start Job")
+        self.but.clicked.connect(self.task_start)
+        self.setCentralWidget(self.but)
 
-        self.progressbar = QProgressBar()
-        self.progressbar.setMinimum(0)
-        self.progressbar.setMaximum(100)
-        self.progressbar.setValue(10)
+        status_label = QLabel("Progress")
+        status_label.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.proggress_bar = QProgressBar()
+        self.proggress_bar.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
 
-        statusBar = QStatusBar()
-        statusBar.addWidget(statusLabel, 1)
-        statusBar.addWidget(self.progressbar, 2)
+        status_bar = QStatusBar()
+        status_bar.addWidget(status_label, 1)
+        status_bar.addWidget(self.proggress_bar, 2)
 
-        self.setStatusBar(statusBar)
+        self.setStatusBar(status_bar)
+
+    def task_def(self):
+        self.task = TaskThread(self)
+        self.task.progressChanged.connect(self.proggress_bar.setValue)
+
+    def task_start(self):
+        self.task.start()
+
+
+class TaskThread(QThread):
+    progressChanged = Signal(int)
+
+    def run(self):
+        for x in range(0, 101):
+            time.sleep(0.1)
+            self.progressChanged.emit(x)
+        self.exit(0)
 
 
 def main():
